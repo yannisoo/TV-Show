e4917b40d624b45b7bf0a5e3d3a6ee0bbaa7ee0d
Yannis <yannis.b8@gmail.com>
2021-10-15 16:00:44 +0200

    removed exception handler

--- TV-Show-Back/src/app.module.ts
+++ TV-Show-Back/src/app.module.ts
@@ -1,39 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ShowModule } from './show/show.module';
 import { UserModule } from './user/user.module';
 
-import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { HttpErrorFilter } from './shared/http-error.filter';
-import { ValidationPipe } from './shared/validation.pipe';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: "postgres",
-    host: "0.0.0.0",
-    username: "admin",
-    password: "admin",
-    synchronize: true,
-    logging: true,
-    database: "demo",
-    port: 5434,
-    entities: ["./**/*.entity.js"]
-  }
-  ), ShowModule, UserModule],
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(), ShowModule, UserModule],
   controllers: [AppController],
   providers: [
-    AppService,    {
-      provide: APP_FILTER,
-      useClass: HttpErrorFilter,
-    },
-    {
-      provide: APP_PIPE,
-      useClass: ValidationPipe,
-    },
+    AppService
   ],
 })
 export class AppModule {}
